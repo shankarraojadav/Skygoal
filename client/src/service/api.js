a58452e0f6da1123ecb2d2249cea34fa945d0c68
SHANKAR JADAV <shankarjadav360@gmail.com>
2023-11-02 12:32:01 +0530

    changes

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:3000";
+const url = "http://localhost:3000" || "https://skygoal-2vfn.onrender.com";
 
 export const signup = async (data) => {
     try {
